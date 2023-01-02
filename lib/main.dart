9a49ac32797c67ac865964acf2a6abf076eb95d9
justyn1303 <justyn1303@gmail.com>
2023-01-02 19:10:16 +0100

    02.01

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:food_allergen_app/screens/home_page_screen.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:food_allergen_app/screens/user_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:food_allergen_app/widgets/login_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+// void main() {
+//   runApp(const MyApp());
+// }
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static final String title = 'Flutter Food Allergen App';
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Food Allergen App',
+  Widget build(BuildContext context) => MaterialApp(
+    navigatorKey: navigatorKey,
+    debugShowCheckedModeBanner: false,
+    title: title,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
       ),
-      home: const UserAccount(),
-    );
-  }
+      home: MainPage(),
+  );
 }
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    body: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasError) {
+          return Center(child: Text('Coś poszło nie tak'));
+        } else if (snapshot.hasData) {
+          return HomePage();
+        } else {
+          return LoginWidget();
+        }
+
+      }
+    )
+  );
+}
+
 
 
---
+++ lib/screens/home_page_screen.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final user = FirebaseAuth.instance.currentUser!;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Strona główna'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              user.email!,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size.fromHeight(50),
+              ),
+              icon: Icon(Icons.arrow_back, size: 32),
+              label: Text(
+                'Wyloguj się',
+                style: TextStyle(fontSize: 24),
+              ),
+              onPressed: () => FirebaseAuth.instance.signOut(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
